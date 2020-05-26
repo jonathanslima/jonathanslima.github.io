a31ff4e4e361fb54f1f364e966e57c63eb5a8619
Lima <jonathan.da.s.lima@accenture.com>
2020-05-25 23:58:13 -0300

    portfolio in js

---
+++ whoAmI.js
@@ -0,0 +1,44 @@
+function whoAmI() {
+  const NAME = "Jonathan Lima";
+  let jobTitle = "Front End Developer";
+
+  let about =
+    "I currently work as a Front End Developer at Accenture. I am responsible for developing new solutions and maintaining projects for large national and international companies.";
+
+  let profile = ["Learner", "Developer", "Futuristic", "Intellection", "Input"];
+
+  let experience = [
+    {
+      company: "Accenture",
+      jobTitle: "Front End Developer",
+      period: "current ",
+    },
+    {
+      company: "Cartello",
+      jobTitle: "Front End Technical Leader",
+      period: "1y",
+    },
+    {
+      company: "Bold",
+      jobTitle: "Front End trainee",
+      period: "1y",
+    },
+  ];
+
+  let skills = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "UX",
+    "React",
+    "Angular",
+    "Vue",
+    "Typescript",
+  ];
+
+  let info = {
+    github: "https://github.com/jonathanslima",
+    linkedin: "https://www.linkedin.com/in/jonathanlima/",
+    site: "https://jonathanslima.guthub.io",
+  };
+}
